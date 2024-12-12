40ddfed705ed717a925948e71a2f803faf535706
unknown <parvinshahanaz375@gmail.com>
2024-12-12 15:29:23 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,34 @@
+// const userFirstName ="Rupama";
+// const userLastName ="Akhter";
+// const userFatherName ="Rashid Howlader";
+// const userMotherName ="Hasina Begum";
+// const userBirthday =15 July;
+// const userCountry ="Bangladesh";
+// const userReligion ="Islam"
+// let userAge = 22;
+// let userCity ="Pirojpur";
+// let userSalary =300000;
+
+// 1.primitive data type ---> string, number, boolean, undefined,null,
+// console.log(typeof(userSalary));
+
+
+
+// const userFastName="Shanaz";
+// const userLastName="Parvin";
+// const usarFatherName="Nazrul islam;"
+// const usarMotherName="Hazarabagum";
+
+
+let firstNumber = 5050;
+let secondNumber = 2345;
+// let remainder =firstNumber - secondNumber;
+
+let firstProduct = 3000;
+let secondProduct = 4000;
+let thirdProduct = 5000;
+let totalPrice = 12000
+let amount = totalPrice / 10;
+console.log(amount);
+let agelist = [22,34,65,76,12,33];
+console.log(agelist);
