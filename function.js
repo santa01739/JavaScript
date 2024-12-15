eabfefcb6d30a1be946e888db8834fa3e926c57e
unknown <parvinshahanaz375@gmail.com>
2024-12-15 16:34:15 +0600

    function add here normal anonymous arrow

---
+++ function.js
@@ -0,0 +1,54 @@
+// // // normal function
+
+// // function sum(x, y){
+// //                     let total = x + y;
+// // return total;
+// // // console.log("line 6:",total);
+// // };
+// // let a = 5;
+// // let b = 7;
+// // let total = sum(a, b);
+// // console.log("line 11:", total);
+
+
+// function fullName(firstName, lastName,age){
+//                     return 'Your first Name'+ ' ' + firstName + ' ' + 'lastName'+ ' ' + lastName + '.Your age'+ ' ' + age;
+// }
+// const yourName = fullName('abir', 'ahmed' , 22);
+// console.log(yourName);
+
+
+// function gold(ana){
+//                     let vori = ana/16;
+//                     return `64 ana =${vori}vori`;
+// }
+// let output = gold(64);
+// console.log(output);
+
+
+// function kilometers(meter){
+//                     let kilometers = meter/1000
+// }
+// let result = kilometers(10000);
+// console.log(result);
+
+
+
+
+
+// // anonymous function
+
+// let sum = function(num1, num2){
+//      return num1 + num2;
+// };
+// console.log(sum(10,20));
+
+
+// arrow function
+
+// let sum = (num1, num2) => num1 + num2;
+let sum = (num1, num2) => {
+                    let total = num1 + num2;
+                    return total * total;
+};
+console.log(sum(10,20));
